98859e780e5a624e23959f52684cf3fc149cc879
root <matus.edgar@gmail.com>
2021-07-05 20:51:16 -0400

    add: springboot initializr -> repo

---
+++ src/main/java/cl/utem/cpyd/z/ProyectoRest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cl.utem.cpyd.z.ProyectoRest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProyectoRestApplication.class);
+	}
+
+}
